41c62c4d241d0f27daac034a166625e6664a0021
zillorz <78161200+Zillorz@users.noreply.github.com>
2026-03-01 16:53:56 -0500

    Fix deno and vtsls
    The nvim-lspconfig attachments are good enough now + deno inlay_hints

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -2,22 +2,46 @@
 require("nvchad.configs.lspconfig").defaults()
 
 -- rustaceanvim replaces rust_analyzer
-local servers = { "html", "cssls", "basedpyright", "clangd", "dockerls", "emmet_language_server", "jsonls", "marksman", "matlab_ls", "svelte", "taplo", "yamlls", "gopls", "denols", "vtsls", "jdtls"
+local servers = {
+  "html",
+  "cssls",
+  "basedpyright",
+  "clangd",
+  "dockerls",
+  "emmet_language_server",
+  "jsonls",
+  "marksman",
+  "matlab_ls",
+  "svelte",
+  "taplo",
+  "yamlls",
+  "gopls",
+  "denols",
+  "vtsls",
+  "jdtls",
 }
 
+-- See more inlay_hint configs here
+-- https://github.com/MysticalDevil/inlay-hints.nvim
 vim.lsp.config("denols", {
-  root_markers = { "deno.json", "deno.jsonc" },
-  workspace_required = true
+  settings = {
+    deno = {
+      inlayHints = {
+        parameterNames = { enabled = "all", suppressWhenArgumentMatchesName = true },
+        parameterTypes = { enabled = true },
+        variableTypes = { enabled = true, suppressWhenTypeMatchesName = true },
+        propertyDeclarationTypes = { enabled = true },
+        functionLikeReturnTypes = { enable = true },
+        enumMemberValues = { enabled = true },
+      },
+    },
+  }
 })
 
-vim.lsp.config("vtsls", {
-  root_markers = { 'package.json', 'tsconfig.json' },
-  workspace_required = true
-})
-
-local mason = vim.fn.stdpath("data") .. "/mason/packages"
+-- the rest of the code below is just for java!
+local mason = vim.fn.stdpath "data" .. "/mason/packages"
 local bundles = {
-  vim.fn.glob(mason .. "/java-debug-adapter/extension/server/com.microsoft.java.debug.plugin-*.jar", true)
+  vim.fn.glob(mason .. "/java-debug-adapter/extension/server/com.microsoft.java.debug.plugin-*.jar", true),
 }
 
 local jars = vim.split(vim.fn.glob(mason .. "/java-test/extension/server/*.jar", true), "\n")
@@ -33,20 +57,20 @@ for _, jar in ipairs(jars) do
 end
 
 -- require('java').setup()
-vim.lsp.config('jdtls', {
+vim.lsp.config("jdtls", {
   init_options = {
-     bundles = bundles,
+    bundles = bundles,
   },
   flag = {
-    allow_incremental_sync = true
+    allow_incremental_sync = true,
   },
   cmd = {
     "jdtls",
     "-data",
-    vim.fn.stdpath('cache') .. '/jdtls/' .. vim.fn.fnamemodify(vim.fn.getcwd(), ":p:h:t")
+    vim.fn.stdpath "cache" .. "/jdtls/" .. vim.fn.fnamemodify(vim.fn.getcwd(), ":p:h:t"),
   },
-  settings = { java = { } },
-  root_markers = {'.classpath', '.project', '.git', 'gradlew', 'mvnw' }
+  settings = { java = {} },
+  root_markers = { ".classpath", ".project", ".git", "gradlew", "mvnw" },
 })
 
-vim.lsp.enable(servers);
+vim.lsp.enable(servers)
